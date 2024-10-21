c3152ed0de8e82906564e44e1d773f323bf25693
Kuramych <vsewslod@yandex.ru>
2024-10-21 18:17:23 +0300

    test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(4);
     }
 }
